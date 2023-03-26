a0b0c0e41889fbc21e11a7b7841da3d9c7d2ebac
raflianggoro <karyabangaping@gmail.com>
2023-03-26 22:48:48 +0700

    main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -325,13 +325,13 @@ img {
                   </ol>
                   <div class="carousel-inner">
                     <div class="carousel-item active">
-                      <img class="d-block w-100" src="..." alt="First slide">
+                      <img class="d-block w-100" src="https://pedulimasjid.com/wp-content/uploads/2023/03/banner-utama-1.jpg" alt="First slide">
                     </div>
                     <div class="carousel-item">
-                      <img class="d-block w-100" src="..." alt="Second slide">
+                      <img class="d-block w-100" src="https://pedulimasjid.com/wp-content/uploads/2023/03/banner-utama-2.jpg" alt="Second slide">
                     </div>
                     <div class="carousel-item">
-                      <img class="d-block w-100" src="..." alt="Third slide">
+                      <img class="d-block w-100" src="https://pedulimasjid.com/wp-content/uploads/2023/03/banner-utama-3.jpg" alt="Third slide">
                     </div>
                   </div>
                   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
