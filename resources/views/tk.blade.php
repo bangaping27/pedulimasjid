7fed5baa356d9a0269c7b582b8abcf22985fcea2
AkbarArnanto <129712041+AkbarArnanto@users.noreply.github.com>
2023-04-10 18:08:58 +0700

    bar

--- resources/views/tk.blade.php
+++ resources/views/tk.blade.php
@@ -318,7 +318,7 @@ img {
         <main>
             <!--=============== HOME ===============-->
             <section class="container section section__height" id="home">
-                <h2 class="section__title">Tentang Kami</h2>
+                <h4 class="section__title">Tentang Kami</h4>
             </section>
 
             {{-- <!--=============== ABOUT ===============-->
