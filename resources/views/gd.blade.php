93764e60ab203a80081dc9f615086e13dc70264c
AkbarArnanto <129712041+AkbarArnanto@users.noreply.github.com>
2023-04-11 10:50:08 +0700

    bar

--- resources/views/gd.blade.php
+++ resources/views/gd.blade.php
@@ -325,6 +325,9 @@ img {
 
                   <div class="card">
                     <div class="card-header">Buat Galang Dana</div>
+                      <div class="mt-10">
+                        <button type="button" class="btn btn-primary btn-block btn-lg">Primary</button>
+                      </div>
                     <div class="card-body">Ini adalah isi dari card</div>
                 </div>
 
